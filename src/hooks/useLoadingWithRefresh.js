009e5e37bfa09a8be152b6b6d2138ed460ded05f
Yash Kalavadiya <yashkalavadiya1010@gmail.com>
2022-07-30 15:36:50 +0530

    done with signup validation and UI fixes

--- src/hooks/useLoadingWithRefresh.js
+++ src/hooks/useLoadingWithRefresh.js
@@ -1,42 +1,41 @@
-import axios from 'axios';
-import {useState, useEffect} from 'react';
-import {useDispatch} from 'react-redux';
-import {setUser} from '../store/authSlice';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setUser } from "../store/authSlice";
 
+const REACT_APP_API_URL = "http://localhost:5500";
 
+export function useLoadingWithRefresh() {
+    const [loading, setLoading] = useState(true);
+    const dispatch = useDispatch();
 
-const REACT_APP_API_URL = 'http://localhost:5500';
+    useEffect(() => {
+        (async () => {
+            try {
+                const { data } = await axios.get(
+                    `${REACT_APP_API_URL}/api/refresh`,
+                    {
+                        withCredentials: true,
+                    }
+                );
+                console.log(data);
 
+                setTimeout(() => {
+                    console.log(data);
+                    dispatch(setUser(data));
+                    setLoading(false);
+                }, 5000);
+            } catch (err) {
+                // console.log(err);
+                // setLoading(false);
+                setTimeout(() => {
+                    console.log(err);
 
+                    setLoading(false);
+                }, 5000);
+            }
+        })();
+    }, []);
 
-export function useLoadingWithRefresh(){
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
-
-
-  useEffect(() => {
-    (async () => {
-      try{
-        const {data} = await axios.get(`${REACT_APP_API_URL}/api/refresh`,{
-          withCredentials: true,
-        });
-        setTimeout(() => {
-          console.log(data);
-          dispatch(setUser(data));
-          setLoading(false);
-        }, 5000);
-        
-      }catch(err){
-        // console.log(err);
-        // setLoading(false);
-        setTimeout(() => {
-          console.log(err);
-          
-          setLoading(false);
-        }, 5000);
-      }
-    })();
-  }, []);
-
-  return {loading};
+    return { loading };
 }
