8067bfeaadb8938a032387769e0902b9413f22f6
GauravChawda <grchawda16@gmail.com>
2022-09-14 18:25:32 +0530

    changed ip

--- src/http/apis.js
+++ src/http/apis.js
@@ -1,45 +1,45 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const REACT_APP_API_URL = "http://localhost:5500";
+export const REACT_APP_API_URL = "http://15.206.27.190:5500";
 
 const api = axios.create({
-    baseURL: REACT_APP_API_URL,
-    withCredentials: true,
-    headers: {
-        "Content-type": "application/json",
-        Accept: "application/json",
-    },
+	baseURL: REACT_APP_API_URL,
+	withCredentials: true,
+	headers: {
+		"Content-type": "application/json",
+		Accept: "application/json",
+	},
 });
 
 //List of all endpoints
 
 export {
-    register,
-    login,
-    requestResetPassword,
-    passwordReset,
-    logUserOut,
-    refresh,
+	register,
+	login,
+	requestResetPassword,
+	passwordReset,
+	logUserOut,
+	refresh,
 } from "./authCalls";
 export {
-    updateCartItem,
-    deleteCartItem,
-    addProductToCart,
-    getProductsInCart,
+	updateCartItem,
+	deleteCartItem,
+	addProductToCart,
+	getProductsInCart,
 } from "./cartCalls";
 export { getCouponData } from "./couponCalls";
 
 export {
-    getShippingDetails,
-    getBillingDetails,
-    uploadShippingDetails,
-    uploadBillingDetails,
-    updateBillingDetails,
-    updateShippingDetails,
-    createOrder,
-    updateBillingAndShippingAsSame,
-    uploadBillingAndShippingAsSame,
+	getShippingDetails,
+	getBillingDetails,
+	uploadShippingDetails,
+	uploadBillingDetails,
+	updateBillingDetails,
+	updateShippingDetails,
+	createOrder,
+	updateBillingAndShippingAsSame,
+	uploadBillingAndShippingAsSame,
 } from "./checkoutCalls";
 
 export { getAllOrders } from "./ordersCalls";
@@ -47,34 +47,34 @@ export { getAllOrders } from "./ordersCalls";
 export { saveContactData, subscribeToNewsletter } from "./miscellaneousCalls";
 
 api.interceptors.response.use(
-    (config) => {
-        return config;
-    },
-    async (error) => {
-        const originalRequest = error.config;
-        if (
-            error.response.status === 401 &&
-            originalRequest &&
-            !originalRequest._isRetry
-        ) {
-            originalRequest.isRetry = true;
-            try {
-                await axios.get(`${REACT_APP_API_URL}/api/refresh`, {
-                    withCredentials: true,
-                });
+	(config) => {
+		return config;
+	},
+	async (error) => {
+		const originalRequest = error.config;
+		if (
+			error.response.status === 401 &&
+			originalRequest &&
+			!originalRequest._isRetry
+		) {
+			originalRequest.isRetry = true;
+			try {
+				await axios.get(`${REACT_APP_API_URL}/api/refresh`, {
+					withCredentials: true,
+				});
 
-                return api.request(originalRequest);
-            } catch (err) {
-                console.log(err.message);
-            }
-        }
+				return api.request(originalRequest);
+			} catch (err) {
+				console.log(err.message);
+			}
+		}
 
-        if (error.response.status === 400) {
-            console.log(error.response.data.message);
-            // toast.error(error.response.data.message);
-        }
-        throw error;
-    }
+		if (error.response.status === 400) {
+			console.log(error.response.data.message);
+			// toast.error(error.response.data.message);
+		}
+		throw error;
+	}
 );
 
 export default api;
