e779b7fa0bd493e16a12c5262daa32553fb23c53
Yash Kalavadiya <yashkalavadiya1010@gmail.com>
2022-09-17 12:37:32 +0530

    validation fix

--- src/http/authCalls.js
+++ src/http/authCalls.js
@@ -18,6 +18,6 @@ export const passwordReset = (data) =>
         }
     );
 
-export const logUserOut = () => api.get("/api/logout");
+export const logUserOut = () => api.post("/api/logout");
 
 export const refresh = () => api.get("/api/refresh");
