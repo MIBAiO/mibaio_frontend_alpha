646c80db682505b93f8197f349a6416f8ee1ac43
Ayush-Bulbule <ayushbulbule24@gmail.com>
2023-10-16 23:17:03 +0530

    Shipping Email Bug Fix

--- src/components/CheckoutAddressForm.js
+++ src/components/CheckoutAddressForm.js
@@ -104,6 +104,7 @@ const CheckoutAddressForm = ({
                 ...errors,
                 zip: false,
             });
+            setFormData({ ...formData, zip: pincode });
         }
 
     }
